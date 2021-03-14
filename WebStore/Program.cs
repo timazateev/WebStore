26a3e60500078c687b2ece9638ebad7299c72dcf
Timofey Zateev <timazateev@gmail.com>
2021-03-14 16:51:06 +0300

    Greetings from config added.

--- WebStore/Program.cs
+++ WebStore/Program.cs
@@ -11,16 +11,13 @@ namespace WebStore
 {
     public class Program
     {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+        public static void Main(string[] args) => CreateHostBuilder(args).Build().Run();
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+                .ConfigureWebHostDefaults(host => host
+                    .UseStartup<Startup>()
+                )
+            ;
     }
 }
