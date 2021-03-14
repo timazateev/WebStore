fd6f7308e987024809efe6bdb43ad456ee913773
Timofey Zateev <timazateev@gmail.com>
2021-03-14 21:41:16 +0300

    Add view for employees table

---
+++ WebStore/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Models
+{
+    public class Employee
+    {
+        public int id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Patronymic { get; set; }
+        public int Age { get; set; }
+    }
+}
