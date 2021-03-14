98748d5ac5f31ec17c230aa7d9154a891ec4e770
Timofey Zateev <timazateev@gmail.com>
2021-03-14 18:20:51 +0300

    Add first view

---
+++ WebStore/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+
+        public IActionResult SecondAction(string id) => Content($"Action with value id:{id}");
+    }
+}
