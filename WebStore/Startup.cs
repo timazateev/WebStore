df89b432f01044715a7f05e2a112f4cc9bed472d
Timofey Zateev <timazateev@gmail.com>
2021-03-20 23:17:04 +0300

    Service registrated in service container

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -9,6 +9,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebStore.Infrastructure.Conventions;
+using WebStore.Infrastructure.Services;
+using WebStore.Infrastructure.Services.Interfaces;
 
 namespace WebStore
 {
@@ -21,6 +23,7 @@ namespace WebStore
         //}
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IEmployeesData, InMemoryEmployeesData>();
             //services AddMvc();
             
             services
