36a0cedb251c1fa62b33194dd3e2b90a2a7b10ae
Timofey Zateev <timazateev@gmail.com>
2021-03-25 18:10:32 +0300

    Filtering products in service

--- WebStore/Infrastructure/Services/InMemoryProductData.cs
+++ WebStore/Infrastructure/Services/InMemoryProductData.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using WebStore.Data;
 using WebStore.Infrastructure.Services.Interfaces;
+using WebStoreDomain;
 using WebStoreDomain.Entities;
 
 namespace WebStore.Infrastructure.Services
@@ -14,5 +15,17 @@ namespace WebStore.Infrastructure.Services
         public IEnumerable<Section> GetSections() => TestData.Sections;
         public IEnumerable<Brand> GetBrands() => TestData.Brands;
 
+        public IEnumerable<Product> GetProducts(ProductFilter Filter = null)
+        {
+            var query = TestData.Products;
+
+            if (Filter?.SectionId is { } section_id)
+                query = query.Where(product => product.SectionId == section_id);
+
+            if(Filter?.BrandId is { } brand_id)
+                query = query.Where(product => product.BrandId == brand_id);
+
+            return query;
+        }
     }
 }
--- WebStore/Infrastructure/Services/Interfaces/IProductData.cs
+++ WebStore/Infrastructure/Services/Interfaces/IProductData.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebStoreDomain;
 using WebStoreDomain.Entities;
 
 namespace WebStore.Infrastructure.Services.Interfaces
@@ -11,5 +12,7 @@ namespace WebStore.Infrastructure.Services.Interfaces
         IEnumerable<Section> GetSections();
 
         IEnumerable<Brand> GetBrands();
+
+        IEnumerable<Product> GetProducts(ProductFilter Filter = null);
     }
 }
