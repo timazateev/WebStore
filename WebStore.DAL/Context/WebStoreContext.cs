09535a3c36c37046d03a66e89f1baf6b6aa2b092
timazateev <39564713+timazateev@users.noreply.github.com>
2021-04-04 20:30:35 +0300

    IdentityDbContext added

--- WebStore.DAL/Context/WebStoreContext.cs
+++ WebStore.DAL/Context/WebStoreContext.cs
@@ -1,14 +1,16 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using WebStoreDomain.Entities;
+using WebStoreDomain.Entities.Identity;
 
 namespace WebStore.DAL.Context
 {
-    public class WebStoreContext : DbContext
+    public class WebStoreContext : IdentityDbContext<User, Role, string>
     {
         public WebStoreContext(DbContextOptions<WebStoreContext> options) : base(options)
         { 
