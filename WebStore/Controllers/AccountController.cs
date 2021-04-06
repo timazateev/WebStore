2605cbf80a5066f673c55b00dac4d8f7919c45a1
timazateev <39564713+timazateev@users.noreply.github.com>
2021-04-06 18:14:17 +0300

    Draft for account controller

---
+++ WebStore/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Register() => View();
+        public IActionResult Login() => View();
+        public IActionResult Logout() => RedirectToAction("Index", "Home");
+        public IActionResult AccessDenied() => View();
+
+    }
+}
