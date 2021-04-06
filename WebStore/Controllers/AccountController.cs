61190a495cede7feba63de626fd6fa268b8028a9
timazateev <39564713+timazateev@users.noreply.github.com>
2021-04-06 23:17:13 +0300

    New user registration

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -1,14 +1,50 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using WebStore.ViewModels;
+using WebStoreDomain.Entities.Identity;
 
 namespace WebStore.Controllers
 {
     public class AccountController : Controller
     {
-        public IActionResult Register() => View();
+        private readonly UserManager<User> _userManager;
+        private readonly SignInManager<User> _signInManager;
+
+        public AccountController(UserManager<User> UserManager, SignInManager<User> SignInManager)
+        {
+            _userManager = UserManager;
+            _signInManager = SignInManager;
+        }
+
+        #region Register
+
+        public IActionResult Register() => View(new RegisterUserViewModel());
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterUserViewModel Model)
+        {
+            if (!ModelState.IsValid) return View(Model);
+
+            var user = new User
+            {
+                UserName = Model.UserName
+            };
+
+            var registration_result = await _userManager.CreateAsync(user, Model.Password);
+            if (registration_result.Succeeded)
+            {
+                await _signInManager.SignInAsync(user, false);
+                return RedirectToAction("Index", "Home");
+            }
+
+            foreach (var error in registration_result.Errors)
+                ModelState.AddModelError("", error.Description);
+
+            return View(Model);
+        }
+        #endregion
+
         public IActionResult Login() => View();
         public IActionResult Logout() => RedirectToAction("Index", "Home");
         public IActionResult AccessDenied() => View();
---
+++ WebStore/ViewModels/RegisterUserViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebStore.ViewModels
+{
+    public class RegisterUserViewModel
+    {
+        [Required, MaxLength(256)]
+        [Display(Name = "User Name")]
+        public string UserName { get; init; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; init; }
+
+        [Required]
+        [Display(Name = "Password Confirmation")]
+        [DataType(DataType.Password)]
+        [Compare(nameof(Password))]
+        public string PasswordConfirm { get; init; }
+
+    }
+}
