eaf8ec399666cbf8d5485ae8782983863d863779
timazateev <39564713+timazateev@users.noreply.github.com>
2021-04-13 14:12:25 +0300

    Logging for registration, login, logout

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Linq;
 using System.Threading.Tasks;
 using WebStore.ViewModels;
 using WebStoreDomain.Entities.Identity;
@@ -10,11 +12,16 @@ namespace WebStore.Controllers
     {
         private readonly UserManager<User> _userManager;
         private readonly SignInManager<User> _signInManager;
+        private readonly ILogger<AccountController> _logger;
 
-        public AccountController(UserManager<User> UserManager, SignInManager<User> SignInManager)
+        public AccountController(
+            UserManager<User> UserManager,
+            SignInManager<User> SignInManager,
+            ILogger<AccountController> Logger)
         {
             _userManager = UserManager;
             _signInManager = SignInManager;
+            _logger = Logger;
         }
 
         #region Register
@@ -26,18 +33,30 @@ namespace WebStore.Controllers
         {
             if (!ModelState.IsValid) return View(Model);
 
+           
+
             var user = new User
             {
                 UserName = Model.UserName
             };
 
+            _logger.LogInformation("User registration {0}", user.UserName);
+
             var registration_result = await _userManager.CreateAsync(user, Model.Password);
             if (registration_result.Succeeded)
             {
+                _logger.LogInformation("User registered {0}", user.UserName);
                 await _userManager.AddToRoleAsync(user, Role.Users);
+                _logger.LogInformation("User {0} added to role {1}  ", user.UserName, Role.Users);
                 await _signInManager.SignInAsync(user, false);
+                _logger.LogInformation("User {0} has login", user.UserName);
                 return RedirectToAction("Index", "Home");
             }
+
+            _logger.LogWarning("Error during user {0} registration: {1}", 
+                user.UserName, 
+                string.Join(",", registration_result.Errors.Select(e => e.Description))
+                );
 
             foreach (var error in registration_result.Errors)
                 ModelState.AddModelError("", error.Description);
@@ -51,6 +70,7 @@ namespace WebStore.Controllers
         [HttpPost]
         public async Task<IActionResult> Login(LoginViewModel Model)
         {
+
             if (!ModelState.IsValid) return View(Model);
 
             var login_result = await _signInManager.PasswordSignInAsync(
@@ -66,12 +86,13 @@ namespace WebStore.Controllers
                 );
             if (login_result.Succeeded)
             {
+                _logger.LogInformation("User {0} has login", Model.UserName);
                 //if (Url.IsLocalUrl(Model.ReturnUrl))
                 //    return Redirect(Model.ReturnUrl);
                 //return RedirectToAction("Index", "Home");
                 return LocalRedirect(Model.ReturnUrl ?? "/");
             }
-
+            _logger.LogWarning("Error during user login {0} or password authentication", Model.UserName);
             ModelState.AddModelError("", "Incorrect user name or password!");
             return View(Model);
         }
@@ -79,7 +100,11 @@ namespace WebStore.Controllers
 
         public async Task<IActionResult> Logout()
         {
+            var user_name = User.Identity.Name;
             await _signInManager.SignOutAsync();
+
+            _logger.LogInformation("User {0} has logout", user_name);
+
             return RedirectToAction("Index", "Home");
         }
 
