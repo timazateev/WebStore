252e6f01d04e6c66e31af63ad4be50a2a0b7b0c8
timazateev <39564713+timazateev@users.noreply.github.com>
2021-04-20 21:36:03 +0300

    Cart service logic and views

--- WebStore/Controllers/CartController.cs
+++ WebStore/Controllers/CartController.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebStore.Infrastructure.Services.Interfaces;
+using WebStore.ViewModels;
 
 namespace WebStore.Controllers
 {
@@ -12,7 +14,7 @@ namespace WebStore.Controllers
         private readonly ICartServices _cartServices;
 
         public CartController(ICartServices CartServices) => _cartServices = CartServices;
-        public IActionResult Index() => View(_cartServices.GetViewModel());
+        public IActionResult Index() => View(new CartOrderViewModel { Cart = _cartServices.GetViewModel() } );
 
         public IActionResult Add(int id)
         {
@@ -36,6 +38,34 @@ namespace WebStore.Controllers
         {
             _cartServices.Clear();
             return RedirectToAction(nameof(Index));
+        }
+
+        [Authorize]
+        public async Task<IActionResult> CheckOut(OrderViewModel OrderModel, [FromServices] IOrderService OrderService)
+        {
+            if (!ModelState.IsValid)
+                return View(nameof(Index), new CartOrderViewModel
+                {
+                    Cart = _cartServices.GetViewModel(),
+                    Order = OrderModel
+                });
+
+            var order = await OrderService.CreateOrder(
+                User.Identity!.Name,
+                _cartServices.GetViewModel(),
+                OrderModel
+                );
+
+            _cartServices.Clear();
+
+            return RedirectToAction(nameof(OrderConfirmed), new { order.id });
+
+        }
+
+        public IActionResult OrderConfirmed(int id)
+        {
+            ViewBag.OrderId = id;
+            return View();
         }
     }
 }
