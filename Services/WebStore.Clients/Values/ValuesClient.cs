fe76ce58b1426ac4f2b5db26f3c35f5590d014cd
timazateev <39564713+timazateev@users.noreply.github.com>
2021-05-02 12:19:23 +0300

    Test service interface

--- Services/WebStore.Clients/Values/ValuesClient.cs
+++ Services/WebStore.Clients/Values/ValuesClient.cs
@@ -1,9 +1,12 @@
-﻿using WebStore.Clients.Base;
+﻿using Microsoft.Extensions.Configuration;
+using WebStore.Clients.Base;
 
 namespace WebStore.Clients.Values
 {
-    class ValuesClient : BaseClient
+    public class ValuesClient : BaseClient
     {
+        public ValuesClient(IConfiguration Configuration) : base(Configuration, "api/values") { }
+
 
     }
 }
