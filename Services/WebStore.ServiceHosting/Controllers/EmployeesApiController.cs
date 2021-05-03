6a8ea39a1ac0b1a8345b7b2297898bc493bfe253
timazateev <39564713+timazateev@users.noreply.github.com>
2021-05-03 18:05:45 +0300

    Controller WebAPI for employees managment

---
+++ Services/WebStore.ServiceHosting/Controllers/EmployeesApiController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebStore.Infrastructure.Services.Interfaces;
+using WebStore.Models;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route("api/employees")]
+    [ApiController]
+    public class EmployeesApiController : Controller, IEmployeesData
+    {
+        private readonly IEmployeesData _EmployeesData;
+
+        public EmployeesApiController(IEmployeesData EmployeesData) => _EmployeesData = EmployeesData;
+
+        [HttpGet] // http://localhost:5001/api/employees
+        public IEnumerable<Employee> Get() => _EmployeesData.Get();
+        
+        [HttpGet("{id}")] //http://localhost:5001/api/employees/5
+        public Employee Get(int id) => _EmployeesData.Get(id);
+
+        [HttpGet("employee")] //http://localhost:5001/api/employees/employee?LastName=Ivanov&FifstName=Ivan&Patronymic=Ivanovich
+        public Employee GetByName(string LastName, string FirstName, string Patronymic) => _EmployeesData.GetByName(LastName, FirstName, Patronymic);
+
+        [HttpPost]
+        public int Add(Employee employee) => _EmployeesData.Add(employee);
+
+        [HttpPost("employee")] //post -> http://localhost:5001/api/employees/employee?LastName=Ivanov&FifstName=Ivan&Patronymic=Ivanovich&Age=33
+        public Employee Add(string LastName, string FirstName, string Patronymic, int Age) => _EmployeesData.Add(LastName, FirstName, Patronymic, Age);
+
+        //[HttpPut("{id}")] // put -> http://localhost:5001/api/employees/5
+        [HttpPut] // put -> http://localhost:5001/api/employees/5
+        public void Update(Employee employee) => _EmployeesData.Update(employee);
+
+        [HttpDelete("{id}")]
+        public bool Delete(int id) => _EmployeesData.Delete(id);
+
+    }
+}
