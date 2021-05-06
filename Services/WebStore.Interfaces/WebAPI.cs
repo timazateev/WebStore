659f621a33e42d114538875f29a29716c064f099
timazateev <39564713+timazateev@users.noreply.github.com>
2021-05-06 15:26:49 +0300

    Web API orders controller

--- Services/WebStore.Interfaces/WebAPI.cs
+++ Services/WebStore.Interfaces/WebAPI.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WebStore.Interfaces
+﻿namespace WebStore.Interfaces
 {
     public static class WebAPI
     {
         public const string TestWebAPI = "api/values";
         public const string Employees = "api/employees";
         public const string Products = "api/products";
+        public const string Orders = "api/orders";
     }
 }
---
+++ Services/WebStore.ServiceHosting/Controllers/OrdersApiController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.DTO;
+using WebStore.Infrastructure.Services.Interfaces;
+using WebStore.Interfaces;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route(WebAPI.Orders)]
+    [ApiController]
+    public class OrdersApiController : ControllerBase, IOrderService
+    {
+        private readonly IOrderService _OrderService;
+
+        public OrdersApiController(IOrderService OrderService) => _OrderService = OrderService;
+
+        [HttpGet("user/{UserName}")]
+        public async Task<IEnumerable<OrderDTO>> GetUserOrders(string UserName) =>
+            await _OrderService.GetUserOrders(UserName);
+
+        [HttpGet("{id:int}")]
+        public async Task<OrderDTO> GetOrderById(int id) => await _OrderService.GetOrderById(id);
+
+        [HttpPost("{UserName}")]
+        public async Task<OrderDTO> CreateOrder(string UserName, [FromBody] CreateOrderModel OrderModel) =>
+            await _OrderService.CreateOrder(UserName, OrderModel);
+    }
+}
