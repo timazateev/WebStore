1b2ded52a527eb8ef1afc58a7afab27636cd829f
timazateev <39564713+timazateev@users.noreply.github.com>
2021-05-07 11:51:53 +0300

    Drafts for identity controllers

---
+++ Services/WebStore.ServiceHosting/Controllers/Identity/RolesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
+using WebStore.Interfaces;
+
+namespace WebStore.ServiceHosting.Controllers.Identity
+{
+    [Route(WebAPI.Identity.Role)]
+    [ApiController]
+    public class RolesController : ControllerBase
+    {
+        private readonly RoleStore<Role> _RoleStore;
+
+        public RolesController(WebStoreContext db)
+        {
+            _RoleStore = new RoleStore<Role>(db);
+        }
+    }
+}
---
+++ Services/WebStore.ServiceHosting/Controllers/Identity/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.DAL.Context;
+using WebStore.Domain.Entities.Identity;
+using WebStore.Interfaces;
+
+namespace WebStore.ServiceHosting.Controllers.Identity
+{
+    [Route(WebAPI.Identity.User)]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserStore<User, Role, WebStoreContext> _UserStore;
+
+        public UsersController(WebStoreContext db)
+        {
+            _UserStore = new UserStore<User, Role, WebStoreContext>(db);
+            //_UserStore.AutoSaveChanges = false;  //default true
+        }
+    }
+}
