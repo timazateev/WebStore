40f18a2d913e6e4f62f515c59fc0d53cd8a1c280
timazateev <39564713+timazateev@users.noreply.github.com>
2021-05-07 11:36:04 +0300

    DTO models for WebAPI Identity. User, Claim

---
+++ Common/WebStoreDomain/DTO/Identity/ClaimDTO.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebStore.Domain.DTO.Identity
+{
+    public abstract class ClaimDTO : UserDTO
+    {
+        public IEnumerable<Claim> Claims { get; set; }
+    }
+
+    public class AddClaimDTO : ClaimDTO { }
+    
+    public class RemoveClaimDTO : ClaimDTO { }
+
+    public class ReplaceClaimDTO : UserDTO
+    { 
+        public Claim Claim { get; set; }
+
+        public Claim NewClaim { get; set; }
+    }
+
+
+}
