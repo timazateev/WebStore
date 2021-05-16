d09f1652c7814187a3909af0456a56a72f33c63a
timazateev <39564713+timazateev@users.noreply.github.com>
2021-05-16 17:54:36 +0300

    Tests project added

---
+++ Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebStore.Controllers;
+
+using Assert = Xunit.Assert;
+
+namespace WebStore.Tests.Controllers
+{
+    [TestClass]
+    public class HomeControllerTests
+    {
+        [TestMethod]
+        public void Index_Returns_View()
+        {
+            var controller = new HomeController();
+
+            var result = controller.Index();
+
+            //Assert.IsInstanceOfType(result, typeof(ViewResult));  // using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+            Assert.IsType<ViewResult>(result);
+        }
+    }
+}
